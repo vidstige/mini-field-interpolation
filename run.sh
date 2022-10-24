b89032023f36b0b270168001eaef354cc62baf91
Samuel Carlsson <samuel.carlsson@volumental.com>
2022-10-24 10:40:44 +0200

    printout before long operation

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ REQUIREMENTS_TXT="requirements.txt"
 if cmp --silent "$VENV_PATH/requirements.txt" "${REQUIREMENTS_TXT}"; then
     source "$VENV_PATH/bin/activate"
 else
+    echo "Creating virtual environment..."
     rm -rf "$VENV_PATH"
     python3 -m venv "$VENV_PATH"
     source "$VENV_PATH/bin/activate"
